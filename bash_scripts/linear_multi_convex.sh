e0fa5621771115532ba3dfa4466b97d824311152
juan carlos perdomo <jcperdomosilva@gmail.com>
2018-04-30 21:20:51 -0700

    changed purpose

--- bash_scripts/linear_multi_convex.sh
+++ bash_scripts/linear_multi_convex.sh
@@ -17,7 +17,7 @@ ALPHA=.5
 LOG_LEVEL="DEBUG"
 MODEL_PATH='linear_models/multi'
 DATA_PATH="linear_experiments_data/multi"
-PURPOSE='debugging'
+PURPOSE='cluster_run_1'
 
 CMD="python -m linear_experiments -exp_type $EXP_TYPE -noise_type $NOISE_TYPE -noise_func $NOISE_FUNC
     -num_classifiers $NUM_CLASSIFIERS -iters $ITERS -alpha $ALPHA -log_level $LOG_LEVEL -model_path $MODEL_PATH
